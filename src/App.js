e4c5ddee6ffe2308e3b1180b061f13a0f8a505a8
Ashok <ashobiz@gmail.com>
2020-12-21 14:28:39 +0530

    Router change.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
+import {HashRouter as Router, Route, NavLink} from 'react-router-dom'
 import Home from './pages/Home'
 import Skills from './pages/Skills'
 import Contact from './pages/Contact'
